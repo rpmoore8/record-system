e5d8d4205202a43ac5b0a55f184561eaad3cd746
Ryan <rpmoore8@neiu.edu>
2019-02-20 12:57:16 -0600

    Added functions for parsing text into record objects

--- cli/actions/fileHandler.js
+++ cli/actions/fileHandler.js
@@ -1,19 +1,74 @@
-// Functions for importing files using the file system
+// Functions for importing files from a directory and parsing them into a list of record objects
 
 const fs = require("fs");
+
+const convertFilesToRecords = directory => {
+  let files = getFiles(directory);
+  let text = readFiles(files, directory);
+  let recordAttributes = parseForAttributes(text);
+  let records = createRecords(recordAttributes);
+
+  return records;
+};
 
 const getFiles = directory => {
   return fs.readdirSync(directory);
 };
 
 const readFiles = (files, directory) => {
-  let records = {};
+  let text = {};
   files.forEach(file => {
     contents = fs.readFileSync(directory + file, "utf8");
-    records[file] = contents;
+    text[file] = contents;
+  });
+  return text;
+};
+
+const parseForAttributes = text => {
+  let recordAttributes = [];
+  for (let file of Object.keys(text)) {
+    let content = text[file];
+    let lines = content.split("\n");
+    lines.forEach(line => {
+      const charsToRemove = /(,|\| )/g;
+      let strippedLine = line.replace(charsToRemove, "");
+      let attributes = strippedLine.split(" ");
+      if (attributes.length == 5) {
+        recordAttributes.push(attributes);
+      } else {
+        // Handle incompatibility error: wrong number of attributes
+      }
+    });
+  }
+  return recordAttributes;
+};
+
+const createRecords = recordAttributes => {
+  let records = [];
+  recordAttributes.forEach(attributes => {
+    let record = {
+      firstName: attributes[0],
+      lastName: attributes[1],
+      gender: attributes[2],
+      favoriteColor: attributes[3],
+      dateOfBirth: attributes[4],
+      dateOfBirthValue: function() {
+        let value = 0;
+        let date = this.dateOfBirth.split("/");
+        value +=
+          Number(date[1]) + Number(date[0]) * 100 + Number(date[2]) * 10000;
+        return value;
+      }
+    };
+    records.push(record);
   });
   return records;
 };
 
+// convertFilesToRecords("/Users/RPMOORE/Desktop/record-system/cli/records/");
+
+module.exports.convertFilesToRecords = convertFilesToRecords;
 module.exports.getFiles = getFiles;
 module.exports.readFiles = readFiles;
+module.exports.parseForAttributes = parseForAttributes;
+module.exports.createRecords = createRecords;
---
+++ cli/tests/fileHandler.test.js
@@ -0,0 +1,55 @@
+// Unit tests for fileHandler.js
+
+const fileHandler = require("../actions/fileHandler");
+
+// MUST be set to the absolute path of records folder
+const directory = "/Users/RPMOORE/Desktop/record-system/cli/records/";
+const files = ["comma", "pipe", "space"];
+
+test("gets files from directory", () => {
+  expect(fileHandler.getFiles("records")).toEqual(files);
+});
+
+test("obtains text from files given path", () => {
+  let text = fileHandler.readFiles(files, directory);
+
+  expect(text["comma"]).toContain("Smith, Lucy, Female, Purple, 5/14/1977");
+  expect(text["pipe"]).toContain("Smith | Larry | Male | Blue | 6/3/1976");
+  expect(text["space"]).toContain("Farris Cindy Female Pink 1/23/1963");
+});
+
+test("converts text object into array of each record's attributes", () => {
+  text = fileHandler.readFiles(files, directory);
+  let recordAttributes = fileHandler.parseForAttributes(text);
+
+  expect(recordAttributes.length).toBe(7);
+  expect(recordAttributes[0].length).toBe(5);
+});
+
+test("converts each record's attributes into a record object", () => {
+  recordAttributes = [];
+  recordAttributes.push(["Drew", "Lacy", "Male", "Orange", "1/2/1990"]);
+  let records = fileHandler.createRecords(recordAttributes);
+  let record = records[0];
+
+  expect(record.firstName).toBe("Drew");
+  expect(record.lastName).toBe("Lacy");
+  expect(record.gender).toBe("Male");
+  expect(record.favoriteColor).toBe("Orange");
+  expect(record.dateOfBirth).toBe("1/2/1990");
+  expect(record.dateOfBirthValue()).toBe(19900102);
+});
+
+test("parses records from directory into list of record objects", () => {
+  let records = fileHandler.convertFilesToRecords(directory);
+
+  expect(records.length).toBe(7);
+  records.forEach(record => {
+    expect(typeof record.firstName).toBe("string");
+    expect(typeof record.lastName).toBe("string");
+    expect(typeof record.gender).toBe("string");
+    expect(typeof record.favoriteColor).toBe("string");
+    expect(typeof record.dateOfBirth).toBe("string");
+    expect(typeof record.dateOfBirthValue()).toBe("number");
+  });
+});
